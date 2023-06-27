2dfa2e849285eaba38806727854ad7a74e16a255
akhilesh.j@solutelabs.com <akiijadhav22@gmail.com>
2023-06-28 02:23:27 +0530

    changed export to export real page -revert this later

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import '../styles/globals.css';
 export { default as i18n } from '../i18n';
 export { default as useLocale } from '../components/hooks/useLocale';
 export { default as HomePage } from '../pages/index';
-export { ReportPage as ReportPageComponent } from '../pages/reports/testt';
+export { ReportPage as ReportPageComponent } from '../pages/reports/index';
