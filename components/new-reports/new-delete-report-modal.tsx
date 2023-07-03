d54a3b8ee61c11803a66be949336428eef857fab
akhilesh.j@solutelabs.com <akiijadhav22@gmail.com>
2023-07-03 14:52:40 +0530

    added functions inside component

--- components/new-reports/new-delete-report-modal.tsx
+++ components/new-reports/new-delete-report-modal.tsx
@@ -11,7 +11,7 @@ import crossIcon from '../../public/icons/cross-icon.svg';
 import { NewReportDetail } from './models/new-report-detail';
 import useRequestUtilities from '../hooks/use-request-utilities';
 import { ReportDetail } from './models/report-details';
-
+import { useRouter } from 'next/navigation';
 const notoSansFont = 'noto-sans';
 
 export default function NewDeleteReportModal(props: {
@@ -20,8 +20,96 @@ export default function NewDeleteReportModal(props: {
   setShow: Dispatch<SetStateAction<boolean>>;
   incomplete?: boolean;
 }) {
+  const router = useRouter();
+  function useRequestUtilities() {
+    const logoutUser = useCallback(() => {
+      localStorage.removeItem('userInfo');
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('applicationKey');
+      router.push('/login');
+    }, []);
+
+    const fetchWrapper = useCallback(async function (props: {
+      url: RequestInfo | URL;
+      method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+      includeAuthToken?: boolean;
+      body?: any;
+      contentType?: string;
+      applicationIdentifier?: string;
+      initiate?: () => any;
+      handleResponse: (response: Response) => any;
+      handleError: (error: any) => any;
+      handleFinally?: () => any;
+      applicationKey?: string;
+    }) {
+      const {
+        url,
+        method = 'GET',
+        includeAuthToken = false,
+        body,
+        initiate,
+        handleResponse,
+        handleError,
+        handleFinally,
+        applicationKey,
+      } = props;
+      const options: RequestInit = {
+        method,
+      };
+      if (includeAuthToken || body) {
+        const headersInit: HeadersInit = {};
+        options.headers = headersInit;
+        if (body) {
+          if (body instanceof FormData) {
+            options.body = body;
+          } else {
+            options.headers['Content-Type'] =
+              props.contentType || 'application/json';
+
+            options.body = props.contentType ? body : JSON.stringify(body);
+            const applicationKeyFromStorage =
+              localStorage.getItem('applicationKey');
+            if (!applicationKeyFromStorage) {
+              options.headers['x-api-key'] = applicationKey;
+            } else {
+              options.headers['x-api-key'] = applicationKeyFromStorage;
+            }
+          }
+        }
+        if (includeAuthToken) {
+          options.headers.Authorization = `Bearer ${localStorage.getItem(
+            'accessToken',
+          )}`;
+        }
+      }
+      if (initiate) {
+        initiate();
+      }
+      try {
+        const response = await fetch(url, options);
+
+        if (includeAuthToken && response.status === 401) {
+          return;
+        }
+        handleResponse(response);
+      } catch (error) {
+        handleError(error);
+      } finally {
+        if (handleFinally) {
+          handleFinally();
+        }
+      }
+    },
+    []);
+
+    return {
+      nextJsRouter: router,
+      logoutUser,
+      fetchWrapper,
+    };
+  }
   const { reportData, show, setShow, incomplete = false } = props;
-  const { fetchWrapper, nextJsRouter: router } = useRequestUtilities();
+  const { fetchWrapper } = useRequestUtilities();
   const [isDeletingReport, setIsDeletingReport] = useState(false);
   const [responseError, setResponseError] = useState('');
 
